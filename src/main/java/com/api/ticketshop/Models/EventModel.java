e11859c399ff6fabaf82916065e08bf18cd5cda2
Gustavo Jorge <gustavojorge080@gmail.com>
2022-05-23 20:27:46 -0300

    class Event

---
+++ src/main/java/com/api/ticketshop/Models/EventModel.java
@@ -0,0 +1,104 @@
+package com.api.ticketshop.Models;
+
+import com.sun.istack.NotNull;
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /*@OneToMany
+    @JoinColumn(name = "billing_address_id")
+    private List<BillingAddressModel> billingAddress;*/
+
+    private Integer billing_address_id;
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private String password;
+
+    private String cpf;
+
+    private String tel;
+
+    private Integer Type;
+
+    public Integer getBilling_address_id() {
+        return billing_address_id;
+    }
+
+    public void setBilling_address_id(Integer billing_address_id) {
+        this.billing_address_id = billing_address_id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public Integer getType() {
+        return Type;
+    }
+
+    public void setType(Integer type) {
+        Type = type;
+    }
+}
