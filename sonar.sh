172afc8353e989bb141d83297806b57191a63c54
Rodrigo Rocha <rodrigorgs@gmail.com>
2022-05-28 16:54:44 -0300

    Add script para anlisar no SonarCloud

---
+++ sonar.sh
@@ -0,0 +1,1 @@
+mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=ufba-poo-2022-1_equipe9 -Dmaven.test.skip=true -Dsonar.organization=ufba-poo-2022-1 -Dsonar.host.url=https://sonarcloud.io
