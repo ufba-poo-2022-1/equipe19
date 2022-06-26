b9aa0b3ebbeddc2cc24f5e02aac97a6419beb8ae
Beatriz Cerqueira <beatriz_cbj@hotmail.com>
2022-06-26 14:13:07 -0300

    fix repository link in SonarCloud

--- sonar.sh
+++ sonar.sh
@@ -1,1 +1,1 @@
-mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=ufba-poo-2022-1_equipe9 -Dmaven.test.skip=true -Dsonar.organization=ufba-poo-2022-1 -Dsonar.host.url=https://sonarcloud.io
+mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=ufba-poo-2022-1_equipe19 -Dmaven.test.skip=true -Dsonar.organization=ufba-poo-2022-1 -Dsonar.host.url=https://sonarcloud.io
